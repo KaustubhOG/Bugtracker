15e02f7c3b5d1823bd0921090fc5c27101fa7ac2
kaustubhOG <kaustubh.devlop@gmail.com>
2025-08-07 21:09:54 +0530

    done setting a hookform but (hardest part)

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,51 @@
+'use client'
+import React, { useState } from 'react'
+import { TextField, Button } from '@radix-ui/themes'
+import SimpleMDE from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
+import './editor-dark.css'
+import { useForm, Controller } from 'react-hook-form'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
-import React from 'react'
-import { TextArea, TextField, Button } from '@radix-ui/themes'
+interface IssueForm {
+  title: string
+  description: string
+}
 
 const NewIssuePage = () => {
+  const router = useRouter()
+  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const [markdown, setmarkdown] = useState('')
+
   return (
-    <div className="space-y-4"> 
-      <TextField.Root placeholder="Issue Title" />
-      <TextArea placeholder="Issue Description" />
-      <Button>Submit Issue</Button>
-    </div>
+    <form
+      className="space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data)
+        router.push('/issues')
+      })}
+    >
+      <TextField.Root placeholder="Issue Title" {...register('title')} />
+
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE
+            {...field}
+            value={markdown}
+            onChange={(value) => {
+              setmarkdown(value)
+              field.onChange(value)
+            }}
+            options={{ autofocus: true }}
+          />
+        )}
+      />
+
+      <Button type="submit">Submit Issue</Button>
+    </form>
   )
 }
 
